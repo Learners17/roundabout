30dfb55c3186bba15d60db1edc640589b248421c
kobbieno8 <gulbetabel@gmail.com>
2026-09-23 23:06:16 -0700

    header is done

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,34 @@
+ function Header ()
+{
+     return(
+      <div className=" flex flex-center justify-center  ">
+    <div className="flex flex-col  p-3  box-shadow mt-4 bg-gradient-to-b  from-[#e3bd58] via-[#c99f35] to-[#9a7618] min-w-[300px] shadow-[0_8px_5px_rgba(45,25,5,0.65),0_14px_12px_rgba(0,0,0,0.25)] rounded-lg inset-shadow-2xs">
+      <div className="flex flex-row  justify-between "> 
+        <div className="relative h-4 w-4 rounded-full bg-radial from-black-800 to-yellow-800 overflow-hidden">
+  <div className="absolute top-0 bottom-0 left-1/2 w-0.5 bg-black -translate-x-1/2 -rotate-45" />
+</div>
+ <div className="relative h-4 w-4 rounded-full bg-radial from-black-800 to-yellow-800 shadow-xl overflow-hidden">
+  <div className="absolute top-0 bottom-0 left-1/2 w-0.5 bg-black -translate-x-1/2 -rotate-45" />
+</div></div>
+<div className="flex flex-col self-center">
+       <h1 className="text-3xl text-bold text-shadow-2xs "> R o u n d a b o u t</h1>
+       <h2 className="flex flex-row  justify-around w-full tracking-[0.1em]">FAIR ROTATION PICKER</h2>
+    </div>
+    <div className="flex flex-row  justify-between">
+      <div className="relative h-4 w-4 rounded-full bg-radial from-black-800 to-yellow-800 overflow-hidden">
+  <div className="absolute top-0 bottom-0 left-1/2 w-0.5 bg-black -translate-x-1/2 -rotate-45" />
+</div>
+      <div className="relative h-4 w-4 rounded-full bg-radial from-black-800 to-yellow-800 overflow-hidden">
+  <div className="absolute top-0 bottom-0 left-1/2 w-0.5 bg-black -translate-x-1/2 -rotate-45" />
+</div>
+    </div>
+
+     
+     
+    </div>
+    
+    
+    </div>
+)
+}
+export default Header
